80c734b1e3980dfaf4c5dcb10f6358bf22ca6437
YoichiroKimoto <64562711+YoichiroKimoto@users.noreply.github.com>
2022-02-05 18:01:39 +0900

    Final

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -18,7 +18,7 @@ export const UserManagement: VFC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
   const { loginUser } = useLoginUser();
-  console.log(loginUser);
+
   useEffect(() => getUsers(), []);
 
   const onClickUser = useCallback(
@@ -49,7 +49,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
